b32008d54471a77042544bac61baa9475097cc2c
Tuh553 <94439946+Tuh553@users.noreply.github.com>
2022-03-22 12:22:37 +0800

    Add files via upload

--- zuoye2.py
+++ zuoye2.py
@@ -4,9 +4,41 @@ Created on Thu Mar 10 11:24:07 2022
 
 @author: 24879
 """
-#保存百度图片到本地
-import requests #先导入爬虫的库，不然调用不了爬虫的函数
-response = requests.get("https://www.baidu.com/img/baidu_jgylogo3.gif")  #get方法的到图片响应
-file = open("D:\\图片","wb") #打开一个文件,wb表示以二进制格式打开一个文件只用于写入
-file.write(response.content) #写入文件
-file.close()#关闭操作，运行完毕后去你的目录看一眼有没有保存成功
+import numpy as np
+names = np.array(['王微', '肖良英', '方绮雯', '刘旭阳','钱易铭'])
+subjects=np.array(['Math', 'English', 'Python', 'Chinese', 'Art', 'Database', 'Physics'])
+scores= np.array([[70,85,77,90,82,84,89],
+[60,64,80,75,80,92,90], [90,93,88,87,86,90,91],
+[80,82,91,88,83,86,80],[88,72,78,90,91,73,80]])
+#2.1思考题1
+print(subjects[[1,2,4]])
+print(names[-3])
+print(names[2:])
+print(subjects[2:4])
+print(subjects[(subjects=='English')|(subjects=='Physics')])
+#2.1思考题2
+print(scores[[0,3]])
+print(scores[[2,4]][:,(subjects == 'Math') | (subjects == 'Python')])
+print(scores[:,(subjects == 'Math') | (subjects == 'Art')])
+print(scores[(names=='王微') | (names=='刘旭阳')][:,(subjects == 'English') | (subjects == 'Art')])
+#2.1思考题3
+a=np.arange(10,20).reshape(2,5)
+print(a)
+#2.2思考题1
+bonus=np.array([0,-3,0,0,0,0,0])
+print(scores+bonus)
+#2.2思考题2
+print(scores.mean (axis = 1))
+#2.2思考题3
+a=np.random.uniform(-1,1,size=(3,4))
+print(a.sum())
+#综合题
+market=np.array(['大润发','沃尔玛','联华','农工商'])
+fruit=np.array(['苹果','梨','香蕉','橘子','芒果'])
+price=np.random.randint(4,11,size=(4,5))
+price[(market=='大润发'),(fruit=='苹果')]+1
+price[(market=='联华'),(fruit=='香蕉')]+1
+price[(market=='农工商')]-2
+print(price[:,(fruit=='苹果')].mean())
+print(price[:,(fruit=='芒果')].mean())
+print(market[price[:,fruit=='橘子'].argmax()]) 
